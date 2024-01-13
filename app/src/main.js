6a79615feb4aa1ab39d51576c72eebbcaaf5ed8e
cyph404 <nicoleblanchette05@gmail.com>
2024-01-13 15:09:49 -0500

    initial commit

---
+++ app/src/main.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+const toggleOpen = (e) => {
+  e.currentTarget.classList.toggle('open')
+}
+
+const toggleActive = (e) => {
+  if (e.propertyName.includes('flex')) {
+    e.currentTarget.classList.toggle('open-active');
+  }
+}
+
+
+// panels.addEventListener('click', toggleOpen)
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive))
